231f9f63cda6354ddf52ba1824e3efe7696a6309
jessiefff <jiefangli0605@gmail.com>
2022-04-26 20:42:48 -0400

    update

---
+++ components/ItemDetail.js
@@ -0,0 +1,17 @@
+import {React} from "react";
+import {StyleSheet, View} from "react-native";
+import {Button, Text, DataTable} from "react-native-paper";
+// import { useState } from "react/cjs/react.production.min";
+import { useState, useEffect}from 'react';
+
+
+//Author: Jiefang Li
+export default function ItemDetail() {
+
+  return (
+    <View>
+      <Text>Item Detail</Text>
+    </View>
+
+  );
+}
